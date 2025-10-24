3883f98d52b2fe258708a03c72bc85261380d27a
danifabbro <la tua email>
2025-10-24 13:40:26 +0200

    inserito programma pizza delivery

---
+++ pizzaDelivery.py
@@ -0,0 +1,25 @@
+print("Benvenuto al Pizza delivery")
+size = input("Che grandezza di pizza desideri? S, M oppure L: ")
+pepperoni = input("Vuoi del salame in più sulla pizza? S o N: ")
+extra_cheese = input("Vuoi del formaggio extra? S o N: ")
+
+bill = 0
+
+if size == "S": #pizza piccola
+    bill = 15
+    if pepperoni == "S": #dico si al salame in più
+        bill += 2
+        if extra_cheese == "S":  #dico si al formaggio extra
+            bill += 1
+elif size == "M":  #pizza media
+    bill = 20
+    if pepperoni == "S": #dico si al salame in più
+        bill += 3
+elif size == "L":    #pizza grande
+    bill = 25
+    if pepperoni == "S": #dico si al salame in più
+        bill += 3
+        if extra_cheese == "S": #dico si al formaggio extra
+            bill += 1
+
+print(f"La pizza costa {bill} euro")
