6b7dd6c4533c5a823abfe521f9a4714e30fe601c
danifabbro <la tua email>
2025-12-16 11:50:13 +0100

    aggiunte immagini alla logica

--- sasso_carta_forbici.py
+++ sasso_carta_forbici.py
@@ -29,14 +29,12 @@ elementi = [roccia,carta,forbici]
 scelta_pc = random.choice(elementi)
 scelta_giocatore = input("Benvenuto al gioco SASSO CARTA E FORBICI!!! Seleziona 1 per ROCCIA, 2 per CARTA, 3 per FORBICI!!\n")
 
-
-
 if scelta_giocatore == "1":
-    if scelta_pc == roccia:
+    if scelta_pc == elementi[0]:
         print("la tua scelta:\n" + roccia + "\n")
         print("Scelta del computer\n" + scelta_pc + "\n")
         print("Hai scelto ROCCIA e il computer ROCCIA. Pareggio!!\n")
-    elif scelta_pc == carta:
+    elif scelta_pc == elementi[1]:
         print("la tua scelta:\n" + roccia + "\n")
         print("Scelta del computer\n" + scelta_pc + "\n")
         print("Hai scelto ROCCIA e il computer CARTA. Hai perso! :(\n")
@@ -45,11 +43,11 @@ if scelta_giocatore == "1":
         print("Scelta del computer\n" + scelta_pc + "\n")
         print("Hai scelto ROCCIA e il computer FORBICI. Hai vintoooo!!! Congratulazioni!!\n")
 elif scelta_giocatore == "2":
-    if scelta_pc == roccia:
+    if scelta_pc == elementi[0]:
         print("la tua scelta:\n" + carta + "\n")
         print("Scelta del computer\n" + scelta_pc + "\n")
         print("Hai scelto CARTA e il computer ROCCIA. Hai vintoooo!!! Congratulazioni!!\n")
-    elif scelta_pc == carta:
+    elif scelta_pc == elementi[1]:
         print("la tua scelta:\n" + carta + "\n")
         print("Scelta del computer\n" + scelta_pc + "\n")
         print("Hai scelto CARTA e il computer CARTA. Pareggio!!\n")
@@ -58,13 +56,15 @@ elif scelta_giocatore == "2":
         print("Scelta del computer\n" + scelta_pc + "\n")
         print("Hai scelto CARTA e il computer FORBICI. Hai perso! :(\n")
 elif scelta_giocatore == "3":
-    if scelta_pc == roccia:
+    if scelta_pc == elementi[0]:
         print("la tua scelta:\n" + forbici + "\n")
         print("Scelta del computer\n" + scelta_pc + "\n")
         print("Hai scelto FORBICI e il computer ROCCIA. Hai perso! :(\n")
-    elif scelta_pc == carta:
+    elif scelta_pc == elementi[1]:
         print("la tua scelta:\n" + forbici + "\n")
         print("Scelta del computer\n" + scelta_pc + "\n")
         print("Hai scelto FORBICI e il computer CARTA. Hai vintoooo!!! Congratulazioni!!\n")
     else:
+        print("la tua scelta:\n" + forbici + "\n")
+        print("Scelta del computer\n" + scelta_pc + "\n")
         print("Hai scelto FORBICI e il computer FORBICI. Pareggio!!\n")
